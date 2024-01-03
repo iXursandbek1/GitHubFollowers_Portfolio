7667bcee119493a2b4a232e489b12c195ae9346e
Xursandbek <xursandbek06082001@mail.com>
2024-01-03 20:42:19 +0500

    Initial Commit

---
+++ GitHubFollowers_Portfolio/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubFollowers_Portfolio
+//
+//  Created by Xursandbek Qambaraliyev on 03/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
